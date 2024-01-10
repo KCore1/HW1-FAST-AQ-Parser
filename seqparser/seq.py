ce188e53986ef9ce6a32ecf83ff813279b92ef8b
Karson Chrispens <karson.chrispens@ucsf.edu>
2024-01-09 21:10:21 -0800

    transcribe function

--- seqparser/seq.py
+++ seqparser/seq.py
@@ -10,7 +10,11 @@ def transcribe(seq: str, reverse: bool = False) -> str:
     Write a function that will transcribe (replace DNA sequence to RNA
     by replacing all 'T' to 'U') in an input sequence
     """
-    pass
+    if len(set(seq) - set(ALLOWED_NUC)) > 0:
+        raise ValueError("Invalid nucleotide in sequence.")
+    if reverse:
+        return reverse_transcribe(seq)
+    return "".join([TRANSCRIPTION_MAPPING[nuc] for nuc in seq])
 
 def reverse_transcribe(seq: str) -> str:
     """
