cc685f98bf3bea829d62213846f9c49aec323ad8
Karson Chrispens <karson.chrispens@ucsf.edu>
2024-01-09 21:18:56 -0800

    fixed both

--- seqparser/seq.py
+++ seqparser/seq.py
@@ -11,23 +11,25 @@ def transcribe(seq: str, reverse: bool = False) -> str:
     by replacing all 'T' to 'U') in an input sequence
 
     Parameters:
-    seq (str): DNA sequence (or RNA sequence if reverse is True)
-    reverse (bool): if True, will do reverse transcription from RNA to DNA
+    seq (str): DNA sequence 
+    reverse (bool): if True, will call reverse_transcribe
 
     Returns:
-    str: RNA sequence (or DNA sequence if reverse is True)
+    str: RNA sequence
     """
 
     if len(set(seq) - set(ALLOWED_NUC)) > 0:
         raise ValueError("Invalid nucleotide in sequence.")
     if reverse:
-        rev_mapping = {v: k for k, v in TRANSCRIPTION_MAPPING.items()}
-        return "".join([rev_mapping[nuc] for nuc in seq])
+        return reverse_transcribe(seq)
     return "".join([TRANSCRIPTION_MAPPING[nuc] for nuc in seq])
 
 def reverse_transcribe(seq: str) -> str:
     """
-    Write a function that will transcribe an input sequence and reverse
+    Function that will transcribe an input sequence and reverse
     the sequence
+
+    Parameters:
+    seq (str): DNA sequence
     """
-    return transcribe(seq)[::-1]
+    return transcribe(seq, reverse=False)[::-1]
